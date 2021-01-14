7887b76a0e27264b65c28780bb070c02d1080dc4
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-01-14 19:16:53 +0530

    Time: 8 ms (69.53%), Space: 7.2 MB (65.22%) - LeetHub

--- palindromic-substrings/palindromic-substrings.cpp
+++ palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-     int pal(string &s, int lo, int hi) {
-        int res=0;
-        while (lo>=0 && hi<s.size()) {
-            if (s[lo] != s[hi])
-                break;
-​
-            --lo;
-            ++hi;
-            res++;
-        }
-​
-        return res;
-    }
-​
+    
     int countSubstrings(string s) {
-        int ans=0;
-        for(int i=0;i<s.size();i++){
-            ans+=pal(s,i,i);
-            ans+=pal(s,i,i+1);
+        int n=s.size(),ans=0;
+        if(n<=0)
+            return 0;
+        bool t[n][n];
+        memset(t,false,sizeof(t));
+        
+        for(int i=0;i<n;i++){
+            t[i][i]=true;
+            ans++;
+        }
+         for(int i=0;i<n-1;i++){
+            t[i][i+1]=(s[i]==s[i+1]);
+            ans+=t[i][i+1];
+        }
+            
+        for(int i=3;i<=n;i++){
+           for(int j=0,k=j+i-1;k<n;j++,k++){
+               t[j][k]=t[j+1][k-1] && (s[j]==s[k]);
+               ans+=t[j][k];
+           }
         }
         return ans;
     }
