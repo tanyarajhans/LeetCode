3e909b32b3f5b3bf7cc155d24b128e95ff843b23
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-01-21 20:15:49 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (99.21%) - LeetHub

---
+++ h-index/h-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n=citations.size();
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(citations[mid]==(n-mid))
+                return citations[mid];
+            else if(citations[mid]>(n-mid))
+                h=mid-1;
+            else
+                l=mid+1;
+        }
+        return n-l;
+        
+    }
+};
