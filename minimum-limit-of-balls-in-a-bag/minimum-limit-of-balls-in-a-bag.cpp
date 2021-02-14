eea7989e833a692b0f643fe3a5d2c9f127e0207e
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-02-15 01:21:56 +0530

    Time: 136 ms (100.00%), Space: 48.7 MB (40.00%) - LeetHub

--- minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -4,9 +4,12 @@ public:
         int left = 1, right = 1e9;
         while (left < right) {
             int mid = (left + right) / 2, count = 0;
-            for (int a : nums)
-                count += (a - 1) / mid;
-            if (count > maxOperations)
+            for (int a : nums){
+                count += a/mid;
+                if(a%mid)
+                  count++;
+            }
+            if (count > nums.size() + maxOperations)
                 left = mid + 1;
             else
                 right = mid;
