57fda1f7fa1bf326ed062ea811eb425e3544c50e
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-02-18 00:03:48 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (97.58%) - LeetHub

---
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        int mapping[26];
+        for (int i = 0; i < 26; i++)
+            mapping[order[i] - 'a'] = i;
+        for (string &w : words)
+            for (char &c : w)
+                c = mapping[c - 'a'];
+        return is_sorted(words.begin(), words.end());
+    }
+};
