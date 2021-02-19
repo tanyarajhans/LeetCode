7be6ec998b49bd01627be67caebbc397114908b1
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-02-19 22:50:57 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (52.35%) - LeetHub

--- score-of-parentheses/score-of-parentheses.cpp
+++ score-of-parentheses/score-of-parentheses.cpp
@@ -3,14 +3,14 @@ public:
     int scoreOfParentheses(string S) {
         stack<int> s;
         int ans=0,x=0;
-        for(int i=0;i<S.size();i++){
-            if(S[i]=='(')
-                x++;
-            else {
-                x--;
-                if(S[i-1]=='(')
+        for(int i=0;i<S.size()-1;i++){
+            if(S[i]=='('){
+                if(S[i+1]==')')
                     ans+=1<<x;
-             }
+                x++;
+            }
+            else
+                x--;
         }
         return ans;
     }
