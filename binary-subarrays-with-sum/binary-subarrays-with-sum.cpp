b82a52fe4f08bf68c6e2181fccb7c9ee5af1b030
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-02-26 20:17:17 +0530

    Time: 56 ms (53.49%), Space: 33.6 MB (21.64%) - LeetHub

--- binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& A, int S) {
         int pre[A.size()+1];
-        for(int i=0;i<A.size();i++){
-            pre[i+1]=pre[i]+A[i];
-        }
+        int ans=0,s=0;
         unordered_map<int,int> m;
-        int ans=0;
-        for(int i:pre){
-            ans+=m[i];
-            m[i+S]++;
+        m[0]=1;
+        for(int i=0;i<A.size();i++){
+            s+=A[i];
+            int k=s-S;
+            ans+=m[k];
+            m[s]++;
         }
         return ans;
     }
