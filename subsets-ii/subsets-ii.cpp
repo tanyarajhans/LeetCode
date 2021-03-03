a37d7d618b7818a8d6184c9b261cbdc978a8800a
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-03 22:40:05 +0530

    Time: 4 ms (72.79%), Space: 7.3 MB (95.57%) - LeetHub

--- subsets-ii/subsets-ii.cpp
+++ subsets-ii/subsets-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    
-    void backtrack(int p, vector<int>& nums, vector<int> &v, vector<vector<int>> &ans){
-        for(int i=p;i<nums.size();i++){
-            if((i>p) && nums[i]==nums[i-1]){
-                continue;
-            }
-            v.push_back(nums[i]);
-            ans.push_back(v);
-            backtrack(i+1,nums,v,ans);
-            v.pop_back();
-        }
-    }
-    
-    
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans{vector<int>{}};
-        vector<int> curr;
-        backtrack(0,nums,curr,ans);
-        return ans;
-    }
+    
+    void backtrack(int p, vector<int>& nums, vector<int> &v, vector<vector<int>> &ans){
+        for(int i=p;i<nums.size();i++){
+            if((i>p) && nums[i]==nums[i-1]){
+                continue;
+            }
+            v.push_back(nums[i]);
+            ans.push_back(v);
+            backtrack(i+1,nums,v,ans);
+            v.pop_back();
+        }
+    }
+    
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        ans.push_back(curr);
+        backtrack(0,nums,curr,ans);
+        return ans;
+    }
 };
