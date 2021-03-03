543e78276e629ab62cec68690b7520755f590b05
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-04 02:39:24 +0530

    Time: 8 ms (76.45%), Space: 9.7 MB (45.43%) - LeetHub

--- permutations-ii/permutations-ii.cpp
+++ permutations-ii/permutations-ii.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-     void backtrack(vector<vector<int>> &t,vector<int> &ans, vector<int>& nums, vector<bool> &vis){
-       if(ans.size()==nums.size())
-           t.push_back(ans);
-       
-      for(int i=0;i<nums.size();i++){
-            if(vis[i] || ((i>0 && nums[i]==nums[i-1]) && !vis[i-1]))
-               continue;
-               vis[i]=true;
-               ans.push_back(nums[i]);
-               backtrack(t, ans, nums, vis);
-               vis[i]=false;
-               ans.pop_back();
-           
-       }
-       
-   }
-    
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-       vector<vector<int>> t;
-       vector<int> ans;
-       vector<bool> vis(nums.size());
-       backtrack(t, ans, nums, vis);
-       return t; 
-    }
+     bool visit[9];
+     void backtrack(set<vector<int>> &t,vector<int> &ans, vector<int>& nums){
+       if(ans.size()==nums.size()){
+          t.insert(ans);
+           return;
+       }
+       
+      for(int i=0;i<nums.size();i++){
+           if(visit[i] || (i>0 && nums[i] == nums[i-1] && visit[i-1] == false))
+                continue;
+               ans.push_back(nums[i]);
+               visit[i]=true;
+               backtrack(t, ans, nums);
+               ans.pop_back();
+               visit[i]=false;
+           }
+       
+       
+   }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+       set<vector<int>> t;
+       vector<int> ans;
+        vector<vector<int>> v;
+       backtrack(t, ans, nums);
+       for(auto it: t)
+           v.push_back(it);
+       return v; 
+    }
 };
