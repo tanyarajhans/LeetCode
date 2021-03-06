3d961aef9fffa373117f25261c204ca31577d67c
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-06 18:48:39 +0530

    Time: 100 ms (86.25%), Space: 53.4 MB (63.41%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.cpp
+++ palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-     bool isPalindrome(string s, int low, int high) {
-        while (low < high) {
-            if (s[low++] != s[high--]) return false;
-        }
-        return true;
-    }
-    
-    void backtrack(vector<vector<string>> &ans, vector<string> &t, int p, string &s){
-        if(p==s.size())
-            ans.push_back(t);
-        for(int i=p;i<s.size();i++){
-           if(isPalindrome(s,p,i)){
-               t.push_back(s.substr(p,i-p+1));
-               backtrack(ans,t,i+1,s);
-               t.pop_back();
-           }
-        }
-        
-    }
-    
-    
-    
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> t;
-        backtrack(ans,t,0,s);
-        return ans;
-    }
+     bool isPalindrome(string s, int low, int high) {
+        while (low < high) {
+            if (s[low++] != s[high--]) return false;
+        }
+        return true;
+    }
+    
+    void backtrack(vector<vector<string>> &ans, vector<string> &t, int p, string &s){
+        if(p==s.size())
+            ans.push_back(t);
+        for(int i=p;i<s.size();i++){
+           if(isPalindrome(s,p,i)){
+               t.push_back(s.substr(p,i-p+1));
+               backtrack(ans,t,i+1,s);
+               t.pop_back();
+           }
+        }
+        
+    }
+    
+    
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> t;
+        backtrack(ans,t,0,s);
+        return ans;
+    }
 };
