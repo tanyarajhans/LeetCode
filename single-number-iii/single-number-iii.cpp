8166f036a30b88095bbffeb168132c5848a2d947
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-13 16:10:59 +0530

    Time: 8 ms (92.11%), Space: 9.9 MB (94.55%) - LeetHub

--- single-number-iii/single-number-iii.cpp
+++ single-number-iii/single-number-iii.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    vector<int> singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        long long x=0;
-        for(int i : nums)
-            x^=i;
-        int k=(x & (x-1))^x;
-        vector<int>res;
-        int c=0;
-        for(int i : nums){
-            if(i & k)
-                c^=i;
-        }
-        res.push_back(c);
-        res.push_back(c^x);
-        return res;
-    }
+    vector<int> singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        long long xorr=0;
+        for(auto i: nums)
+            xorr^=i;
+        vector<int> ans(2);
+        xorr &= -xorr;
+        int p=0,q=0;
+        for(auto i: nums){
+            if(i&xorr)
+                p^=i;
+            else
+                q^=i;
+        }
+        ans[0]=p;
+        ans[1]=q;
+        return ans;
+    }
 };
