39d89768a5113a17784289648d13cbef35aa6e58
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-22 15:21:05 +0530

    Time: 8 ms (90.12%), Space: 9.1 MB (77.51%) - LeetHub

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    int ans=0;
-    
-    void backtrack(int i, vector<int>& nums, long long sum, int S){
-        if(i==nums.size()){
-            if(S==sum)
-            ans++;
-        }
-        else{
-            backtrack(i+1, nums, sum+nums[i], S);
-            backtrack(i+1, nums, sum-nums[i], S);
-        }
-        
-    }
-    
     int findTargetSumWays(vector<int>& nums, int S) {
-        backtrack(0, nums, 0, S);
-        return ans;
+        int n=nums.size();
+        int sum=0,s=0,z=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(nums[i]==0)
+                z++;
+        }
+        if(S>sum)
+            return 0;
+        if((S+sum)%2!=0)
+            return 0;
+        s=(sum+S)/2;
+        int t[n+1][s+1];
+        memset(t,0,sizeof(t));
+        for(int i=0;i<n+1;i++){
+            t[i][0]=1;
+        }
+        for(int i=0;i<s+1;i++){
+            t[0][i]=0;
+        }
+        t[0][0]=1;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<s+1;j++){
+               if(nums[i-1]==0)
+                   t[i][j]=t[i-1][j];
+               else if(nums[i-1]<=j)
+                   t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        return (1<<z)*t[n][s];
     }
 };
