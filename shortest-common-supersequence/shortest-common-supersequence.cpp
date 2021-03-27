f2909fe329656874642b374bf77498986ed3af31
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-27 13:16:25 +0530

    Time: 8 ms (96.21%), Space: 10.5 MB (85.88%) - LeetHub

--- shortest-common-supersequence/shortest-common-supersequence.cpp
+++ shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.size();
-        int m=str2.size();
-        int t[n+1][m+1];
-        memset(t,0,sizeof(t));
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(str1[i-1]==str2[j-1])
-                    t[i][j]=1+t[i-1][j-1];
-                else
-                    t[i][j]=max(t[i][j-1], t[i-1][j]);
-            }
-        }
-        string ans;
-        int i=n,j=m;
-        while(i>0 && j>0){
-            if(str1[i-1]==str2[j-1]){
-                ans+=str1[i-1];
-                i--;
-                j--;
-            }
-            else{
-                if(t[i-1][j]>t[i][j-1]){
-                  ans+=str1[i-1];
-                  i--;  
-                }
-                else{
-                  ans+=str2[j-1];
-                  j--;  
-                }
-            }
-        }
-        while(i>0)
-        {
-            ans+=str1[i-1];
-            i--;
-        }
-         while(j>0)
-        {
-            ans+=str2[j-1];
-            j--;
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
+    string shortestCommonSupersequence(string a, string b) {
+        int m=a.size();
+        int n=b.size();
+        int t[m+1][n+1];
+        for(int i=0;i<=m;i++)
+            t[i][0]=0;
+        for(int i=0;i<=n;i++)
+            t[0][i]=0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(a[i-1]==b[j-1])
+                    t[i][j]=1+t[i-1][j-1];
+                else
+                    t[i][j]=max(t[i-1][j], t[i][j-1]);
+            }
+        }
+        int i=m,j=n;
+        string ans;
+        while(i>0 && j>0){
+            if(a[i-1]==b[j-1]){
+                ans.push_back(a[i-1]);
+                i--;
+                j--;
+            }
+            else if(t[i-1][j]>t[i][j-1]){
+                ans.push_back(a[i-1]);
+                i--;
+            }
+            else{
+                ans.push_back(b[j-1]);
+                j--;
+            }   
+        }
+        while(i>0){
+           ans.push_back(a[i-1]);
+           i--; 
+        }
+         while(j>0){
+           ans.push_back(b[j-1]);
+           j--; 
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
 };
