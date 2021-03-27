bfe9356dfb9fbb93bd1f1a22442ea27ab9d6e5e7
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-27 17:17:08 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (82.45%) - LeetHub

--- distinct-subsequences/distinct-subsequences.cpp
+++ distinct-subsequences/distinct-subsequences.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int dp[1002][1002];
-    
-    int solve(string s, string t, int i, int j){
-        if(i==0 && j==0)
-            return 1;
-        if(j==0)
-            return 1;
-        if(i==0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(s[i-1]==t[j-1])
-           return dp[i][j]=solve(s,t,i-1,j-1)+solve(s,t,i-1,j);
-        else
-            return dp[i][j]=solve(s,t,i-1,j);
-    }
-    
-    int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(s,t,n,m);
-    }
+    int numDistinct(string s, string t) {
+        int m=s.size();
+        int n=t.size();
+        long long dp[m+1][n+1];
+        dp[0][0]=1;
+        for(int i=1;i<=m;i++)
+            dp[i][0]=1;
+        for(int i=1;i<=n;i++)
+            dp[0][i]=0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                    dp[i][j]=dp[i-1][j]; 
+            }
+        }
+        return dp[m][n];
+    }
 };
