823e5ef77f6e355dc9ec99633762c2765ebb1b8d
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-06-08 01:02:57 +0530

    Time: 4 ms (64.89%), Space: 7.6 MB (60.16%) - LeetHub

--- unique-paths-ii/unique-paths-ii.cpp
+++ unique-paths-ii/unique-paths-ii.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid.size();
-        int m=obstacleGrid[0].size();
-        
-        int t[n][m];
-        memset(t,0,sizeof(t));
-        
-        if(obstacleGrid[0][0]==1)
-            return 0;
-        
-         for(int i=0;i<m;i++){
-            if(obstacleGrid[0][i]==0)
-                t[0][i]=1;
-            else break;
-        }
-        
-        for(int i=0;i<n;i++){
-            if(obstacleGrid[i][0]==0)
-                t[i][0]=1;
-            else break;
-        }
-        
-        
-         for(int i=1;i<n;i++){
-              for(int j=1;j<m;j++){
-                if(obstacleGrid[i][j]==0){
-                    t[i][j]=t[i-1][j]+t[i][j-1];
-                }
-                  else t[i][j]=0;
-              }
-         }
-         return t[n-1][m-1];
-        
-    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        long long t[n][m];
+        memset(t,0,sizeof(t));
+        if(grid[n-1][m-1]==1)
+            return 0;
+        t[n-1][m-1]=1;
+        for(int i=n-2;i>=0;i--){
+            if(grid[i][m-1]==0)
+                t[i][m-1]=t[i+1][m-1];
+            else
+                t[i][m-1]=0;
+        }
+        for(int i=m-2;i>=0;i--){
+            if(grid[n-1][i]==0)
+                t[n-1][i]=t[n-1][i+1];
+            else
+                t[n-1][i]=0;
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2; j>=0;j--){
+                if(grid[i][j]==0)
+                    t[i][j]=t[i+1][j]+t[i][j+1];
+                else
+                    t[i][j]=0;
+            }
+        }
+        return t[0][0];
+    }
 };
