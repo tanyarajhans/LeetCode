de8b737d8a55541c35fd4e5eacabca02f366dfd5
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-06-07 13:51:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.91%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1 || n==2)
-            return n;
-        int t[n+1];
-        t[0]=1;
-        t[1]=1;
-        t[2]=2;
+        int a[n+1];
+        a[0]=0;
+        a[1]=1;
+        if(n==1)
+            return 1;
+        a[2]=2;
+        if(n==2)
+            return 2;
         for(int i=3;i<=n;i++)
-            t[i]=t[i-1]+t[i-2];
-        return t[n];
+            a[i]=a[i-1]+a[i-2];
+        return a[n];
     }
 };
