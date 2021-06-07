cf279ce0351fa8242aa2d26dca6c20f07232e367
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-06-07 14:18:36 +0530

    Time: 8 ms (27.83%), Space: 8.3 MB (71.49%) - LeetHub

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>a;
-        a=triangle;
         int n=triangle.size();
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<a[i].size();j++)
-            {
-                int x=INT_MAX,y=-1;
-                if(i-1>=0&&j<a[i-1].size())
-                {
-                    x=a[i-1][j];
-                }
-                if(i-1>=0&&j-1>=0)
-                {
-                    x=min(x,a[i-1][j-1]);
-                }
-                if(x!=INT_MAX)
-                    a[i][j]+=x;
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                triangle[i][j]+=min(triangle[i+1][j], triangle[i+1][j+1]);
             }
         }
-        int min=INT_MAX;
-        for(int i=0;i<a.size();i++)
-        {
-            if(a[a.size()-1][i]<min)
-                min=a[a.size()-1][i];
-        }
-        return min;
+        return triangle[0][0];
     }
 };
