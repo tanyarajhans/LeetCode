71ab67500cb6300c621a34ce9635c6ac9ae3dbec
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-06-08 16:55:20 +0530

    Time: 128 ms (72.69%), Space: 75.1 MB (98.30%) - LeetHub

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,23 +2,33 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        int left[n],right[n];
-        int ans=0,mini=INT_MAX;
+        // int left[n],right[n];
+        // int ans=0,mini=INT_MAX;
+        // for(int i=0;i<n;i++){
+        //     mini=min(mini,prices[i]);
+        //     ans=max(ans,prices[i]-mini);
+        //     left[i]=ans;
+        // }
+        // int maxi=INT_MIN;
+        // ans=0;
+        // for(int i=n-1;i>=0;i--){
+        //     maxi=max(maxi,prices[i]);
+        //     ans=max(ans,maxi-prices[i]);
+        //     right[i]=ans;
+        // }
+        // for(int i=0;i<n;i++) {
+        //     ans = max(ans, left[i] + right[i]);
+        // }
+        // return ans;
+        
+        int b1=INT_MAX, b2=INT_MAX;
+        int s1=0,s2=0;
         for(int i=0;i<n;i++){
-            mini=min(mini,prices[i]);
-            ans=max(ans,prices[i]-mini);
-            left[i]=ans;
+            b1=min(b1, prices[i]);
+            s1=max(s1, prices[i]-b1);
+            b2=min(b2, prices[i]-s1);
+            s2=max(s2, prices[i]-b2);
         }
-        int maxi=INT_MIN;
-        ans=0;
-        for(int i=n-1;i>=0;i--){
-            maxi=max(maxi,prices[i]);
-            ans=max(ans,maxi-prices[i]);
-            right[i]=ans;
-        }
-        for(int i=0;i<n;i++) {
-            ans = max(ans, left[i] + right[i]);
-        }
-        return ans;
+        return s2;
     }
 };
