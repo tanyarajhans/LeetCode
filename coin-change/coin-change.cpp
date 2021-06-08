5ce8c9ecec22b7a121e003ac4e0967fb9298179b
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-06-08 13:38:11 +0530

    Time: 92 ms (39.49%), Space: 10 MB (95.47%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -5,16 +5,17 @@ public:
         int s=amount;
         
         int t[s+1];
-        for(int i=1;i<=s;i++)
-            t[i]=INT_MAX-1;
+        
         
         t[0]=0;
         
         for(int i=1;i<=s;i++){
+            int mini=INT_MAX-1;
             for(int j=0;j<n;j++){
                 if(coins[j]<=i && t[i-coins[j]]!=INT_MAX-1)
-                    t[i]=min(t[i],t[i-coins[j]] +1);
+                    mini=min(mini,t[i-coins[j]] +1);
             }
+            t[i]=mini;
         }
        return t[s]>s? -1: t[s];
     }
