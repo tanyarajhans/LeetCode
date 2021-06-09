3c5c1f0d97e345e972d79477a31ab37701dd7000
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-06-09 12:45:46 +0530

    Time: 160 ms (23.96%), Space: 12.8 MB (94.61%) - LeetHub

--- number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-   
-    int findNumberOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        if(n<=1)
-            return n;
-        vector<int> l(n,1);
-        vector<int> c(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                
-                if(l[j]>=l[i]){
-                    l[i]=l[j]+1;
-                    c[i]=c[j];
-                }
-                else if(l[j]+1==l[i]){
-                    c[i]+=c[j];
-                }
-              }
-            }
-        }
-        
-        int maxl=0,ans=0;
-        for(int i=0;i<n;i++)
-            maxl=max(maxl,l[i]);
-        for(int i=0;i<n;i++){
-            if(l[i]==maxl)
-            {
-                ans+=c[i];
-              
-            }
-        }
-        return ans;
-    }
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+            return 1;
+        int dp[n];
+        int count[n];
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=1;
+            count[i]=1;
+        }
+        int ans=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(dp[j]+1>dp[i] && nums[j]<nums[i]){
+                    dp[i]=dp[j]+1;
+                    count[i]=count[j];
+                }
+                else if(dp[j]+1==dp[i] && nums[j]<nums[i])
+                    count[i]+=count[j];
+            }
+        }
+        for(int i=0;i<n;i++)
+            ans=max(ans, dp[i]);
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(ans==dp[i])
+                c+=count[i];
+        }
+        return c;
+    }
 };
