04759f90938e5c2cac60ba785be1d92a46abbf73
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-07-09 20:18:17 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (63.02%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        if(matrix.size()==0)
-            return ans;
-        int r1=0,r2=matrix.size()-1;
-        int c1=0,c2=matrix[0].size()-1;
-        
-        while(r1<=r2 && c1<=c2){
-            for(int c=c1;c<=c2;c++){
-                ans.push_back(matrix[r1][c]);
-            }
-            for(int r=r1+1;r<=r2;r++){
-                ans.push_back(matrix[r][c2]);
-            }
-            if(r1<r2 && c1<c2){
-                for(int c=c2-1;c>c1;c--){
-                  ans.push_back(matrix[r2][c]);  
-                }
-                for(int r=r2;r>r1;r--){
-                  ans.push_back(matrix[r][c1]);  
-                }
-            }
-            r1++;
-            r2--;
-            c1++;
-            c2--;
-        }
-        return ans;
-    }
+    vector<int> ans;
+    
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int r1=0, r2=n-1, c1=0, c2=m-1;
+        while(r1<=r2 && c1<=c2){
+            for(int c=c1;c<=c2;c++)
+                ans.push_back(mat[r1][c]);
+            for(int r=r1+1;r<=r2;r++)
+                ans.push_back(mat[r][c2]);
+            if(r1<r2 && c1<c2){
+                for(int c=c2-1;c>c1;c--)
+                    ans.push_back(mat[r2][c]);
+                for(int r=r2;r>r1;r--)
+                    ans.push_back(mat[r][c1]);
+            }
+            
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
 };
