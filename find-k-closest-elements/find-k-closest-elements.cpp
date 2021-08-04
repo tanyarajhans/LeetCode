0095dc2b415eeaa0aaf0c255c98ea28c72b4b320
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-08-04 20:50:59 +0530

    Time: 40 ms (65.33%), Space: 31.7 MB (63.98%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.cpp
+++ find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n=arr.size();
-        int index=lower_bound(arr.begin(),arr.end(),x)-arr.begin();
-        int l=max(0,index-k),h=min(n-1,index+k-1);
-        vector<int> ans;
-        while(h-l+1>k){
-            if(x-arr[l]>arr[h]-x)
-                l++;
-            else
-                h--;
-        }
-        for(int i=l;i<=h;i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
-    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n=arr.size();
+        int l=0, r=n-1;
+        int ind=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]>x)
+               r=mid-1;
+            else{
+                ind=mid;
+                l=mid+1;
+            }
+        }
+        cout<<ind;
+        l=max(ind-k,0), r=min(ind+k,n-1);
+        vector<int> ans;
+        int c=0;
+        while(r-l>=k){
+            if(abs(arr[l]-x)<=abs(arr[r]-x)){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        for(int i=l;i<=r;i++)
+            ans.push_back(arr[i]);
+        return ans;
+    }
 };
