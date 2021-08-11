ee7efd457b6ad0eb63b61090a0a20d6f762cee25
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-08-11 21:24:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (41.52%) - LeetHub

--- backspace-string-compare/backspace-string-compare.cpp
+++ backspace-string-compare/backspace-string-compare.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    bool backspaceCompare(string S, string T) {
-        int m=S.size(),n=T.size();
-        string s1="",s2="";
-        int skip=0;
-        for(int i=m-1;i>=0;){
-            if(S[i]=='#'){
-                skip++;
-                i--;
-            }
-            else if(skip>0){
-                skip--;
-                i--;
-            }
-            else{
-                s1+=S[i];
-                i--;
-            }
-        }
-        skip=0;
-        for(int i=n-1;i>=0;){
-            if(T[i]=='#'){
-                skip++;
-                i--;
-            }
-            else if(skip>0){
-                skip--;
-                i--;
-            }
-            else{
-                s2+=T[i];
-                i--;
-            }
-        }
-        return (s1==s2);
-    }
+    bool backspaceCompare(string s, string t) {
+        int n=s.size(), m=t.size();
+        int i=n-1, j=m-1;
+        int skips=0, skipt=0;
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s[i]=='#'){
+                    skips++;
+                    i--;
+                }
+                else if(skips>0){
+                    skips--;
+                    i--;
+                }
+                else
+                    break;
+            }
+            while(j>=0){
+                if(t[j]=='#'){
+                    skipt++;
+                    j--;
+                }
+                else if(skipt>0){
+                    skipt--;
+                    j--;
+                }
+                else
+                    break;
+            }
+            if(i>=0 && j>=0 && s[i]!=t[j])
+                return false;
+            if((i>=0) != (j>=0))
+                return false;
+            i--;
+            j--;
+        }
+        return true;
+    }
 };
