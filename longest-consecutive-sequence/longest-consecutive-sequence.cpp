3a101acda0fc7336d206add911b5cdb5a5c44030
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-08-11 22:05:04 +0530

    Time: 108 ms (49.73%), Space: 31.7 MB (23.31%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,60 +1,22 @@
 class Solution {
 public:
-
-int par[100005], size[100005];
-unordered_map<int, int> m;
-
-int find(int u){
-    if(u == par[u])
-        return u;
-    
-    else
-        return par[u]=find(par[u]);
-}
-
-void combine (int u, int v){
-    u = find(u);
-    v = find(v);
-    
-    if(u == v)
-        return;
-    
-    else
-    {
-        if(size[u] > size[v])
-        {
-            par[v] = u;
-            size[u] += size[v];
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        map<int, int> m;
+        int ans=0;
+        for(auto x: nums)
+            m[x]++;
+        for(auto x: m){ // x is start of streak
+            if(!m.count(x.first-1)){ 
+                int curr=x.first;
+                int maxsf=1;
+                while(m.find(curr+1)!=m.end()){
+                    curr++;
+                    maxsf++;
+                }
+            ans=max(ans, maxsf);
+            }
         }
-        
-        else
-        {
-            par[u] = v;
-            size[v] += size[u];
-        }
-    
-    }
-}
-    
-int longestConsecutive(vector<int>& nums) {
-       int n=nums.size();
-       for(int i=0;i<100005;i++){
-           par[i]=i;
-           size[i]=1;
-       }
-       for(int i=0;i<n;i++){
-           if(m.count(nums[i]))
-               continue;
-           if(m.count(nums[i]-1))
-               combine(m[nums[i]-1], i);
-           if(m.count(nums[i]+1))
-               combine(m[nums[i]+1], i);
-           m[nums[i]]=i;
-       }
-       int ans=0;
-       for(int i=0;i<n;i++){
-        ans=max(ans, size[i]);
-       }
-       return ans;
+        return ans;
     }
 };
