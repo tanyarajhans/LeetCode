d271248496ba2528af0441b672c45df5b6d4574d
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-10-28 03:39:01 +0530

    Time: 196 ms (25.22%), Space: 24.9 MB (27.40%) - LeetHub

--- minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
-    int dis[102][102];
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    
     int minCost(vector<vector<int>>& grid) {
-        priority_queue<vector<int>, vector<vector<int> >, greater<vector<int> > > pq;
-        for(int i = 0; i < 102; i++) {
-            for(int j = 0; j < 102; j++) {
-                dis[i][j] = 1e9;
-            }
-        }
-        dis[0][0] = 0;
-        pq.push({ 0, 0, 0 });
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, m+n));
+        dp[0][0] = 0;
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > pq;
+        pq.push({0, 0, 0});
+        
         while(!pq.empty()) {
-            vector<int> cur = pq.top();
-            int x = cur[1], y = cur[2], cst = cur[0];
+            auto v = pq.top();
             pq.pop();
-            if(x == grid.size() - 1 && y == grid[0].size()) {
-                return cst;
-            }
-            if(x + 1 < grid.size()) {
-                int ncst = grid[x][y] == 3 ? cst : cst + 1;
-                if(ncst < dis[x + 1][y]) {
-                    dis[x + 1][y] = ncst;
-                    pq.push({ ncst, x + 1, y });
-                }
-            }
-            if(y + 1 < grid[0].size()) {
-                int ncst = grid[x][y] == 1 ? cst : cst + 1;
-                if(ncst < dis[x][y + 1]) {
-                    dis[x][y + 1] = ncst;
-                    pq.push({ ncst, x, y + 1 });
-                }
-            }
-            if(x - 1 >= 0) {
-                int ncst = grid[x][y] == 4 ? cst : cst + 1;
-                if(ncst < dis[x - 1][y]) {
-                    dis[x - 1][y] = ncst;
-                    pq.push({ ncst, x - 1, y });
-                }
-            }
-            if(y - 1 >= 0) {
-                int ncst = grid[x][y] == 2 ? cst : cst + 1;
-                if(ncst < dis[x][y - 1]) {
-                    dis[x][y - 1] = ncst;
-                    pq.push({ ncst, x, y - 1 });
-                }
+            int r = v[1], c = v[2];
+
+            for(int k=0; k<4; k++) {
+                int nr = r + dx[k];
+                int nc = c + dy[k];
+                int cost = 1;
+                if(k+1 == grid[r][c]) cost = 0;
+                if(nr >= 0 && nc >=0 && nr < m && nc < n && cost + v[0] < dp[nr][nc]) {
+                    dp[nr][nc] = cost + v[0];
+                    pq.push({dp[nr][nc], nr, nc});
+                }    
             }
         }
-        return dis[grid.size() - 1][grid[0].size() - 1];
+            
+        return dp[m-1][n-1];
     }
 };
