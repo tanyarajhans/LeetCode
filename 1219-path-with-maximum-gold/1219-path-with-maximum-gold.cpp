8b04e253974f9d4156bad638f02032f05982ff7a
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-01-23 16:23:33 +0530

    Time: 80 ms (42.39%), Space: 7.2 MB (97.53%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int n,m;
-    int vis[17][17];
     int dx[4]={1,0,-1,0};
     int dy[4]={0,1,0,-1};
     
     int solve(int i, int j, vector<vector<int>>& grid){
         if(i<0 || j<0 || i>=n || j>=m)
             return 0;
-        if(grid[i][j]==0 || vis[i][j])
+        if(grid[i][j]==0)
             return 0;
         int ans=0;
-        ans+=grid[i][j];
-        vis[i][j]=1;
+        int temp=grid[i][j];
+        grid[i][j]=0;
         for(int k=0;k<4;k++){
             int xx=i+dx[k];
             int yy=j+dy[k];
             int c=solve(xx,yy,grid);
-            ans=max(grid[i][j]+c, ans);
+            ans=max(c, ans);
         }
-        vis[i][j]=0;
-        return ans;
+        grid[i][j]=temp;
+        return ans+temp;
     }
     
     int getMaximumGold(vector<vector<int>>& grid) {
         n=grid.size();
         m=grid[0].size();
         int ans=0;
-        memset(vis,0,sizeof(vis));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]>0)
