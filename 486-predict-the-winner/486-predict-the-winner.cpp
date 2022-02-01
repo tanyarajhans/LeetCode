a9969f02948e172e060f7872e3369711b1d22e39
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-02-01 13:42:53 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (67.59%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int dp[22][22];
     
     int solve(int i, int j, vector<int>& nums){ //solving max score for p1
         if(i>j)
             return 0;
         if(i==j)
             return nums[i];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         int x=max(nums[i]+min(solve(i+2, j, nums), solve(i+1,j-1,nums)), nums[j]+min(solve(i+1, j-1, nums), solve(i, j-2, nums)));
-        return x;
+        return dp[i][j]=x;
     }
     
     bool PredictTheWinner(vector<int>& nums) {
@@ -15,6 +18,7 @@ public:
         int total=0;
         for(auto x: nums)
             total+=x;
+        memset(dp,-1,sizeof(dp));
         int p1=solve(0, n-1, nums);
         return p1>=total-p1;
     }
