c8949bf2582035311437d47e39047251acd24db9
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-02-28 11:59:42 +0530

    Time: 24 ms (83.87%), Space: 9 MB (81.47%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -5,30 +5,32 @@ public:
         if(n==1)
             return 0;
         int target = (1<<n)-1;
-        queue<pair<int,pair<int, int>>> q; //id, dist, mask
-        int ans=0;
+        queue<pair<int, int>> q; //id, mask
         int vis[n][target+1];
         memset(vis,0,sizeof(vis));
         for(int i=0;i<n;i++){
-            q.push({i, {0, 1<<i}});
+            q.push({i, 1<<i});
             vis[i][1<<i]=1;
         }
-        
+        int ans=0;
         while(!q.empty()){
-            auto curr=q.front();
-            q.pop();
-            int id=curr.first;
-            int dist=curr.second.first;
-            int state=curr.second.second;
-            for(auto x: graph[id]){
-                int newState = state | (1<<x);
-                if(newState==target)
-                    return dist+1;
-                else if(vis[x][newState]==1)
+            int k=q.size();
+            while(k--){
+                auto curr=q.front();
+                q.pop();
+                int id=curr.first;
+                int state=curr.second;
+                for(auto x: graph[id]){
+                    int newState = state | (1<<x);
+                    if(newState==target)
+                        return ans+1;
+                    else if(vis[x][newState]==1)
                     continue;
-                vis[x][newState]=1;
-                q.push({x, {dist+1,newState}});
+                    vis[x][newState]=1;
+                    q.push({x, newState});
+                }
             }
+            ans++;
         }
         return -1;
     }
