688d7e82ed1fe88a24ef5b98a7533fdd5970ae7b
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-03-06 13:10:01 +0530

    Time: 5 ms (35.85%), Space: 5.8 MB (96.86%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int mod=1e9+7;
-    int dp[502][502];
-    int n;
     
-    long long solve(int leftToPick, int leftToDeliver){
-        if(leftToPick==0 && leftToDeliver==0)
-            return 1;
-        if(leftToPick<0 || leftToDeliver<0){
-            return 0;
+   int countOrders(int n) {
+        long long ans = 1;
+        for (int i = 1; i <= n; ++i) {
+            // Ways to arrange all pickups, 1*2*3*4*5*...*n
+            ans = (ans * i)%mod;
+            // Ways to arrange all deliveries, 1*3*5*...*(2n-1)
+            ans = (ans * (2 * i - 1))%mod;
         }
-        if(dp[leftToPick][leftToDeliver]!=-1)
-            return dp[leftToPick][leftToDeliver];
-        long long ans=0;
-        ans=(ans+leftToPick*solve(leftToPick-1, leftToDeliver+1))%mod; //choose one to pick up, then we get to deliver it as well
-        ans=(ans+leftToDeliver*solve(leftToPick, leftToDeliver-1))%mod; //choose one to deliver
-        return dp[leftToPick][leftToDeliver]=ans;
-    }
-    
-    int countOrders(int n) {
-        n=n;
-        memset(dp,-1,sizeof(dp));
-        return solve(n,0); //we have n pick up choices and 0 delivery choices initially
+        return ans;
     }
 };
