888fd980d5e22cd0720ce2376ae1c2648a639027
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-05-24 18:43:34 +0530

    Time: 7 ms (47.05%), Space: 7 MB (81.53%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[30005]; //dp[i]=longest valid parentheses till index i
+    int longestValidParentheses(string s) {
+        int n=s.size();
+        memset(dp,0,sizeof(dp));
+        if(n==0 || n==1)
+            return 0;
+        for(int i=1;i<n;i++){
+            if(s[i]==')'){
+                if(s[i-1]=='('){
+                    dp[i]=2+(i-2>=0?dp[i-2]:0);
+                }
+                else if((i-dp[i-1]-1>=0) && s[i-dp[i-1]-1]=='('){
+                    dp[i]=2+dp[i-1]+((i-dp[i-1])>=2?dp[i-dp[i-1]-2]:0);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
