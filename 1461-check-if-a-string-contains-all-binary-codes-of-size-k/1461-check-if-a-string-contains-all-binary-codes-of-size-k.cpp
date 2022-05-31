0a7eac15c013e2a6d1d19ab3e0169ef61d874108
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-05-31 23:46:21 +0530

    Time: 47 ms (98.11%), Space: 19 MB (91.51%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
+    //new_hash = ((prev_hash << 1) & all_one) | last_digit_of_new_hash
     bool hasAllCodes(string s, int k) {
-        int n=s.size();
-        int i=0,j=0;
+        // int n=s.size();
+        // int i=0,j=0;
+        // int x=1<<k;
+        // set<string> st;
+        // while(j<n){
+        //     if(j-i+1==k){
+        //         st.insert(s.substr(i,k));
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        // int d=st.size();
+        // return (d==x);
+        
         int x=1<<k;
-        set<string> st;
-        while(j<n){
-            if(j-i+1==k){
-                st.insert(s.substr(i,k));
-                i++;
+        int n=s.size();
+        vector<bool> v(x,false);
+        int hash=0;
+        int maxi=x-1;
+        int c=0;
+        for(int i=0;i<n;i++){
+            hash=((hash<<1) & maxi) | (s[i]-'0');
+            if(i-k+1>=0 && !v[hash]){
+                v[hash]=true;
+                c++;
+                if(c==x)
+                    return true;
             }
-            j++;
         }
-        int d=st.size();
-        return (d==x);
+        return (c==x);
     }
 };
