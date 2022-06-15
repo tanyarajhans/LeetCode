cf5ae831c326a81a1ae9db11fdc75ea42e7797bb
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-06-16 02:46:25 +0530

    Time: 4 ms (67.81%), Space: 7.7 MB (61.64%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     vector<vector<int>> ans;
     int n;
+    int used[11];
     
-    void backtrack(int ind, vector<int>& nums, vector<int> temp){
-        if(ind==n){
-            ans.push_back(temp);
-            return;
+    
+    void backtrack(int ind, vector<int>& nums, vector<int>& temp){
+        ans.push_back(temp); //try generating of every size = 0,1,2,3...
+       // backtrack(ind+1, nums, temp);
+        for(int i=ind;i<n;i++){
+            if(i==ind || nums[i]!=nums[i-1]){ //first element h to daal do uske baad don't add duplicates
+                temp.push_back(nums[i]);
+                backtrack(i+1,nums,temp); //size+1
+                temp.pop_back();
+            }
+            
         }
-        backtrack(ind+1,nums,temp);
-        while(ind+1<n && nums[ind]==nums[ind+1]){
-            temp.push_back(nums[ind]);
-            ind++;
-        }
-        temp.push_back(nums[ind]);
-        backtrack(ind+1, nums, temp);
-        temp.pop_back();
+        
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         n=nums.size();
         int ind=0;
         vector<int> temp;
+        memset(used, 0, sizeof(used));
         sort(nums.begin(), nums.end());
         backtrack(ind, nums, temp);
         return ans;
