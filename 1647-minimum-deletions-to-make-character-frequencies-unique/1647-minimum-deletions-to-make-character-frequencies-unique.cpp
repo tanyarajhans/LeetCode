08d7a15824c7d70cad75655e809516454ff9d16e
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-06-29 02:12:42 +0530

    Time: 101 ms (44.97%), Space: 17.2 MB (81.69%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        map<int,vector<char>> m;
-        int n=s.size();
-        map<char,int> freq;
-        for(auto ch: s){
-            freq[ch]++;
+        vector<int> freq (26, 0);
+        for (char c : s){
+            freq[c-'a']++;
         }
-        for(auto it: freq){
-            m[it.second].push_back(it.first);
-        }
+        sort(freq.begin(), freq.end(), greater<int>());
         int ans=0;
-        map<int,vector<char>>::reverse_iterator it;
-        for (it = m.rbegin(); it!= m.rend(); it++) {
-            int v=it->second.size();
-            if(v<2)
-                continue;
-            for(int j=0;j<v-1;j++){
-                char c=it->second[j];
-                int f=it->first;
-                while(m.count(f) && f>0){
-                    f--;
-                    ans++;
-                }
-                m[f].push_back(c);
+        int maxl=s.size();
+        for(int i=0;i<26 && freq[i]>0;i++) {
+            if(freq[i]>=maxl){
+                ans+=freq[i]-maxl;
+                freq[i]=maxl;
             }
+            maxl=max(0, freq[i]-1);
         }
         return ans;
     }
