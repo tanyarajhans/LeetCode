f91139a87e314b43da5c53d0a94168d0ae0bff7d
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-07-26 03:00:32 +0530

    Time: 996 ms (31.03%), Space: 9.4 MB (67.73%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int vis[17];
+    int n;
+    
+    bool solve(vector<int>& nums, int ind, int cs, int target, int k){
+        if(k==1){
+            return true;
+        }
+        if(ind==n){
+            return false;
+        }
+        if(cs==target)
+            return solve(nums, 0, 0, target, k-1);
+        for(int i=ind;i<n;i++){
+            // if (i-1 >= 0 && nums[i]==nums[i-1] && !vis[i-1]) //handle TLE for consecutive same elements
+            //     continue;
+            if(!vis[i] && cs+nums[i]<=target){
+                vis[i]=1;
+                if(solve(nums,i+1,cs+nums[i],target,k))
+                    return true;
+                vis[i]=0;
+            }
+        }
+        return false;
+    }
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        n=nums.size();
+        memset(vis,0,sizeof(vis));
+        int sum=0;
+        for(auto x: nums)
+            sum+=x;
+        if(k==0 || sum%k!=0)
+            return false;
+        int target=sum/k;
+        int cs=0;
+        int ind=0;
+        return solve(nums, ind, cs, target, k);
+    }
+};
