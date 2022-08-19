db8f2fc56d94c3ae38023ed87986f8252d5084d3
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-08-19 23:18:53 +0530

    Update 387-first-unique-character-in-a-string.cpp

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -3,8 +3,9 @@ public:
     int firstUniqChar(string s) {
         vector<int> v(26,0);
         for(auto x:s){
-            v[x-'a']++;
+            v[x-'a']++; // set total every letter
         }
+        // search letter with total 1
         for(int i=0;i<s.size();i++){
             if(v[s[i]-'a']==1)
                 return i;
