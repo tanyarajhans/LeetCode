4efeb42614c882b84fd4ac9df9cf7ad4f4545f36
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-10-05 01:24:23 +0530

    Time: 32 ms (74.66%), Space: 25.5 MB (29.34%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -13,16 +13,12 @@ class Solution {
 public:
     vector<TreeNode*> ans;
     
-    TreeNode* solve(TreeNode* &root, set<int> &del){
+    void solve(TreeNode* &root, set<int> &del){
         if(!root)
-            return NULL;
-        TreeNode* l=solve(root->left, del);
-        TreeNode* r=solve(root->right, del);
+            return;
+        solve(root->left, del);
+        solve(root->right, del);
         if(del.find(root->val)!=del.end()){
-            if(!root->left && !root->right){
-                root=NULL;
-                return NULL;
-            }
             if(root->left){
                 ans.push_back(root->left);
             }
@@ -31,7 +27,6 @@ public:
             }
             root=NULL;
         }
-        return NULL;   
     }
     
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
