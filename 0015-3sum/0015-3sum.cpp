74c803b2cfdd70e65442a091bfc1623530357a01
Tanya Rajhans <61904667+tanyarajhans@users.noreply.github.com>
2022-11-03 09:31:26 +0530

    Time: 188 ms (48.45%), Space: 21.9 MB (37.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,6 +8,8 @@ public:
             return ans;
         sort(nums.begin(), nums.end());
         for(int i=0;i<n;i++){
+            if (nums[i]>0) 
+                break;
             if(i>0 && nums[i]==nums[i-1])
                 continue;
             int j=i+1,k=n-1;
